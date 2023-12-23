0a26dbf070bfd67e7c4f12f107d51a1a6708fa87
Samual Halder <samual.halder2020@gamil.com>
2023-12-23 22:07:54 +0530

    Day 1: the home page is created

--- src/features/product-list/product-listSlice.js
+++ src/features/product-list/product-listSlice.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { fetchCount } from './counterAPI';
+import { fetchCount } from './product-listAPI';
 
 const initialState = {
   value: 0,
